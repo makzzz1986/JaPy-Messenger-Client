1fa3c713b8010fda7c76ea20eaa4bb577f355f64
Max Pisarenko <pisarenkomakzzz@yandex.ru>
2016-08-08 12:34:50 +0700

    08.08.16 changing nick -finished

--- app/src/main/java/ru/rdtc/makzzz/chat/MainActivity.java
+++ app/src/main/java/ru/rdtc/makzzz/chat/MainActivity.java
@@ -3,7 +3,6 @@ package ru.rdtc.makzzz.chat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Point;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
